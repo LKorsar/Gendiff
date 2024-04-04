8ae2a0c1177806b405e18604492141bd21a77c91
Мария Акимкина <wapovalova@gmail.com>
2024-04-04 16:56:57 +0300

    added __tests__/index.test.js and __fixtures__/fileOutput_.txt

---
+++ __tests__/index.test.js
@@ -0,0 +1,19 @@
+import url from 'url';
+import path, { dirname } from 'node:path';
+import fs from 'fs';
+import gendiff from '../src/index.js';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFile(getFixturePath(filename), 'utf-8');
+
+const file1Output = readFile('fileOutput_.txt');
+
+const file1 = './__fixtures__/file1.json';
+const file2 = './__fixtures__/file2.json';
+
+test('simple using', () => {
+  expect(gendiff(file1, file2)).toEqual(file1Output);
+});
