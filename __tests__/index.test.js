a8480c5ab486d9a910dea8c3b77cba70e69ef063
LKorsar <147144323+LKorsar@users.noreply.github.com>
2024-04-04 17:10:24 +0300

    Update index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,15 +1,16 @@
-import url from 'url';
-import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
 import fs from 'fs';
 import gendiff from '../src/index.js';
 
-const __filename = url.fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFile(getFixturePath(filename), 'utf-8');
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const file1Output = readFile('fileOutput_.txt');
+const file1Output = readFileSync('fileOutput_.txt');
 
 const file1 = './__fixtures__/file1.json';
 const file2 = './__fixtures__/file2.json';
