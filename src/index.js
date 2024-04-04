58f262c6767357a95cb2e1033c4e6bbdf9b5f7b3
Мария Акимкина <wapovalova@gmail.com>
2024-04-04 16:59:33 +0300

    updated src/index.js, src/parse.js, src/stringify.js, bin/gendiff.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import { readFileSync } from 'fs';
 import path from 'path';
 import parseFile from './parse.js';
-import compare from './compare.js';
-import stringifyAndSort from './stringify.js';
+import getDiffTree from './getDiffTree.js';
+import getDiffInformation from './stringify.js';
 
 const gendiff = (filepath1, filepath2) => {
-  const obj1 = parseFile(readFileSync(path.resolve(filepath1)));
-  const obj2 = parseFile(readFileSync(path.resolve(filepath2)));
-  const obj3 = compare(obj1, obj2);
-  const result = stringifyAndSort(obj3);
+  const getFilePath1 = path.resolve(filepath1);
+  const getFilePath2 = path.resolve(filepath2);
+
+  const getFileData1 = readFileSync(getFilePath1);
+  const getFileData2 = readFileSync(getFilePath2);
+
+  const obj1 = parseFile(getFileData1);
+  const obj2 = parseFile(getFileData2);
+
+  const tree = getDiffTree(obj1, obj2);
+  const result = getDiffInformation(tree);
   console.log(result);
 };
 
--- src/stringify.js
+++ src/stringify.js
@@ -1,3 +1,19 @@
-export default (obj) => {
-  return JSON.stringify(obj);
+export default (tree) => {
+  const result = tree.map((item) => {
+    if (item.type === 'added') {
+      return `+ ${item.key}: ${item.value}`;
+    }
+    if (item.type === 'deleted') {
+      return `- ${item.key}: ${item.value}`;
+    }
+    if (item.type === 'changed') {
+      return ` - ${item.key}: ${item.value1}
+      + ${item.key}: ${item.value2}`;
+    }
+    if (item.type === 'unchanged') {
+      return `    ${item.key}: ${item.value}`;
+    }
+    return `  ${item.key}: ${item.value}`;
+  });
+  return result;
 };
