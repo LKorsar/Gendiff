edfffb1820e5ae3d0649f4f9c20681c3306e3ce4
Мария Акимкина <wapovalova@gmail.com>
2024-04-07 17:12:14 +0300

    updated src/index.js

--- src/index.js
+++ src/index.js
@@ -8,13 +8,16 @@ const gendiff = (filepath1, filepath2) => {
   const getFilePath1 = path.resolve(filepath1);
   const getFilePath2 = path.resolve(filepath2);
 
-  const getFileData1 = readFileSync(getFilePath1);
-  const getFileData2 = readFileSync(getFilePath2);
+  const getFileData1 = readFileSync(getFilePath1, 'utf-8');
+  const getFileData2 = readFileSync(getFilePath2, 'utf-8');
 
-  const obj1 = parseFile(getFileData1);
-  const obj2 = parseFile(getFileData2);
+  const formatOfFile1 = path.extname(filepath1);
+  const formatOfFile2 = path.extname(filepath2);
 
-  const tree = getDiffTree(obj1, obj2);
+  const dataOfFile1 = parseFile(getFileData1, formatOfFile1);
+  const dataOfFile2 = parseFile(getFileData2, formatOfFile2);
+
+  const tree = getDiffTree(dataOfFile1, dataOfFile2);
   const result = getDiffInformation(tree);
   console.log(result);
 };
