afc01cead4872951fa06763a89bb89785c8c2943
Мария Акимкина <wapovalova@gmail.com>
2024-04-09 22:52:52 +0300

    updated src/index.js and __tests__/index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,10 +1,10 @@
-import { fileURLToPath } from 'url';
+import url from 'url';
 import path from 'path';
 import fs from 'fs';
 import { test, expect } from '@jest/globals';
 import gendiff from '../src/index.js';
 
-const __filename = fileURLToPath(import.meta.url);
+const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ const gendiff = (filepath1, filepath2) => {
   const getFileData1 = readFileSync(getFilePath1, 'utf-8');
   const getFileData2 = readFileSync(getFilePath2, 'utf-8');
 
-  const formatOfFile1 = path.extname(filepath1);
-  const formatOfFile2 = path.extname(filepath2);
+  const formatOfFile1 = path.extname(filepath1).substring(1);
+  const formatOfFile2 = path.extname(filepath2).substring(1);
 
   const dataOfFile1 = parseFile(getFileData1, formatOfFile1);
   const dataOfFile2 = parseFile(getFileData2, formatOfFile2);
