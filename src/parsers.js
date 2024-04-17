e8a0ddba2586dda4e6d85940f07948cb7aa1baeb
Мария Акимкина <wapovalova@gmail.com>
2024-04-17 16:55:34 +0300

    updated src/parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -4,9 +4,7 @@ export default (data, format) => {
   switch (format) {
     case 'json':
       return JSON.parse(data);
-    case 'yml':
-      return YAML.load(data);
-    case 'yaml':
+    case 'yml' || 'yaml':
       return YAML.load(data);
     default:
       throw new Error(`'Unknown format! ${format}'`);
