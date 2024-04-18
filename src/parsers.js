c4e326d33897e3262e4818f29bb2f46508cd7d54
Мария Акимкина <wapovalova@gmail.com>
2024-04-18 10:19:12 +0300

    updated .github/workflows/gendiff.yml and src/parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -4,7 +4,8 @@ export default (data, format) => {
   switch (format) {
     case 'json':
       return JSON.parse(data);
-    case 'yml' || 'yaml':
+    case 'yml':
+    case 'yaml':
       return YAML.load(data);
     default:
       throw new Error(`'Unknown format! ${format}'`);
